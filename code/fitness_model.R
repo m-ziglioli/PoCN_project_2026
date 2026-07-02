385f67967e03cbfbac38320909876e5e01ed04ed
m-ziglioli <m.ziglio2003@gmail.com>
2026-07-02 22:59:45 +0200

    working generation

--- code/fitness_model.R
+++ code/fitness_model.R
@@ -40,29 +40,49 @@ attraction_probabilities <- function(g) {
         return(1)
     }
     else {
-        V(g)$fitness *degree(g)  / sum(V(g)$fitness * degree(g))
+        V(g)$fitness *degree(g) / sum(V(g)$fitness * degree(g))
     }
 }
 
-create_links <- function(g, m) {
+create_links <- function(g, m, attach_prob) {
+    
+    # sample m nodes among the old nodes V(g)[1:(N-1)]
+    # and attach the new links to them
+    z <- length(V(g)) - 1
+    idxs <- sample(1:z, size=m, replace=TRUE, 
+                    prob=attach_prob)
 
+    chosen_nodes <- V(g)[idxs]
+    new_node <- V(g)[vcount(g)]
+
+    for (j in seq_along(chosen_nodes)) {
+        old_node <- chosen_nodes[j]
+        g <- add_edges(g, c(new_node, old_node))
+    }
+    
+    return(g)
+
+    
 }
 
 main <- function(N,m=1, dt=1, theta=1,  beta=1) {
     # creating first node
     # at time 1, at time 0 no nodes present
-    g <- make_graph(edges=c(), n=1)
+    g <- make_graph(edges=c(), n=1, directed=FALSE)
 
     V(g)$fitness <- draw_fitness(theta, beta)
 
     for (t in seq(2, N, dt)) {
-        g <- add_node(g, theta, beta)
+        nodes_attraction_prob <- attraction_probabilities(g)
 
-        g <- create_links(g, m)
+        g <- add_node(g, theta, beta)
+        g <- create_links(g, m, attach_prob=nodes_attraction_prob)
 
     }
     #print(vcount(g))
     #print(V(g)$fitness)
+
+    return(g)
 }
 
 main(N=10)
