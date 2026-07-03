3265dac993edcea3ed648182d78604fec153a724
m-ziglioli <m.ziglio2003@gmail.com>
2026-07-03 23:27:04 +0200

    working

--- code/main.R
+++ code/main.R
@@ -10,24 +10,22 @@ compute_partition_function <- function(g) {
 }
 
 make_run <- function(N, n_runs, m=1, theta=1, save=TRUE) {
-    mu_mean <- c()
-    mu_std <- c()
 
     # per each beta, run n_runs to do statistical averages
-    for (beta in beta_values) {
-        mu_values <-  c()
+    mu_mean <- numeric(length(beta_values))
+    mu_std  <- numeric(length(beta_values))
 
-        for (j in seq(1, n_runs)) {
-        g <- make_fitness_graph(N, beta=beta, m=m)
-        Z_t <- compute_partition_function(g)
+    for (i in seq_along(beta_values)) {
+        beta <- beta_values[i]
 
-        mu <- Z_t / (m * vcount(g) ) # N=vcount
-        mu_values <- c(mu_values, mu)
-        }
+        mu_values <- replicate(n_runs, {
+            g <- make_fitness_graph(N, beta = beta, m = m)
+            Z_t <- compute_partition_function(g)
+            Z_t / (m * vcount(g))
+        })
 
-        # appending results for this beta
-        mu_mean <- c(mu_mean, mean(mu_values))
-        mu_std <- c(mu_std, sqrt(var(mu_values)))
+        mu_mean[i] <- mean(mu_values)
+        mu_std[i]  <- sd(mu_values)
     }
 
     results <- data.frame (
@@ -48,9 +46,10 @@ make_run <- function(N, n_runs, m=1, theta=1, save=TRUE) {
 ################ initial parameter values
 m <- 1
 theta <- 1
-beta_values <- c(1, 1)#seq(0.1, 50, length.out=50)
+T_values <- seq(0.05, 10, length.out=20)
+beta_values <- 1/T_values   
 
-n_runs <- 1
+n_runs <- 10
 
 res <- make_run(1000, n_runs=n_runs)
 
