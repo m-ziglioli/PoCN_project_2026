6cf210a62e519de52ba8d27ce4634e1297919608
m-ziglioli <m.ziglio2003@gmail.com>
2026-07-04 11:19:47 +0200

    working parallel

--- code/main.R
+++ code/main.R
@@ -11,36 +11,31 @@ make_run <- function(N, n_runs, m=1, theta=1, save=TRUE) {
     mu_mean <- c()
     mu_std <- c()
 
-    # per each beta, run n_runs to do statistical averages
-    for (beta in beta_values) {
-        mu_values <-  c()
-
-        for (j in seq(1, n_runs)) {
-        g <- make_fitness_graph(N, beta=beta, m=m)
+    run_single_simulation <- function(beta, N, m) {
+        # These are the steps that create the graph and compute the partition function
+        g <- make_fitness_graph(N, beta = beta, m = m)
         Z_t <- compute_partition_function(g)
-
-        mu <- Z_t / (m * vcount(g) ) # N=vcount
-        mu_values <- c(mu_values, mu)
-        }
-
-        # appending results for this beta
-        mu_mean <- c(mu_mean, mean(mu_values))
-        mu_std <- c(mu_std, sqrt(var(mu_values)))
+        return(Z_t / (m * vcount(g)))
     }
 
-    results <- data.frame (
-        T_values = 1/beta_values,
-        mu_mean = mu_mean,
-        mu_std = mu_std
-    )
+
+    results <- lapply(beta_values, function(beta) {
+        # mclapply automatically copies the sourced functions to the workers
+        mu_values <- unlist(mclapply(1:n_runs, function(i) {
+            run_single_simulation(beta, N, m)
+        }, mc.cores = num_cores))
+        
+        c(mu_mean = mean(mu_values), mu_std = sqrt(var(mu_values)))
+        })
 
     # beta_values is global..
+    results_df <- data.frame(T_values = 1/beta_values, do.call(rbind, results))
 
     if (save) {
-        write.csv(results, file=paste0("../data/mu_values_N_", N))
+        write.csv(results_df, file=paste0("../data/mu_values_N_", N, ".csv"), row.names=FALSE)
     }
 
-    return(results)
+    return(results_df)
 }
 
 ################################à
@@ -48,16 +43,16 @@ make_run <- function(N, n_runs, m=1, theta=1, save=TRUE) {
 # sources
 source("common.R")
 source("fitness_model.R")
-
 ################ initial parameter values
 
 
 
 m <- 1
 theta <- 1
-beta_values <- c(1, 1)#seq(0.1, 50, length.out=50)
-
-n_runs <- 1
+a <-seq(0.05, 1, length.out=20)
+T_values <- c(a, seq(1, 5, length.out=20))
+beta_values <- 1/T_values
+n_runs <- 10
 #################################################
 # PARALLELIZATION
 
@@ -66,8 +61,7 @@ cl <- makeCluster(num_cores)
 
 # CRITICAL: Export your functions and any global variables the cluster needs
 # Replace 'igraph' with whatever package contains make_fitness_graph if applicable
-clusterEvalQ(cl, library(igraph)) 
-clusterExport(cl, c("run_single_simulation", "make_fitness_graph", "compute_partition_function", "N", "m"))
+
 res <- make_run(1000, n_runs=n_runs)
 
 print(res)
